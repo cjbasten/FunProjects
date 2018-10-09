e9c41349e406a3bd92b6cbfc4e89a641c4f61536
corybasten <coryjbasten@gmail.com>
2018-10-08 19:42:56 -0700

    Nighttime commits

--- largestFile.py
+++ largestFile.py
@@ -4,5 +4,9 @@ import os
 directory = input("Enter the absolute path that you would like to walk through: ")
 for folderName, subfolders, filenames in os.walk(directory):
     for filename in filenames:
-        if os.path.getsize(filename) >= 100:
-            print(filename + ": " + str(os.path.getsize(filename)) + " bytes")
+        try:
+            if os.path.getsize(filename) >= 1000:
+                # Print the the absolute path and filename of files that are at least 1000 bytes.
+                print(folderName + '/' + filename + ": " + str(os.path.getsize(filename)) + " bytes")
+        except FileNotFoundError:
+            continue
