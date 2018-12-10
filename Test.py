23096162f9db49544c7814571021a25baec971c7
corybasten <coryjbasten@gmail.com>
2018-12-09 16:34:07 -0800

    Commits

--- Test.py
+++ Test.py
@@ -7,11 +7,11 @@
 #Phil, how come this^ doesn't print like the one below
 #It was originially just print(col), I added the row, to it for troubleshooting
 
-# a = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
-# for row in a:
-#     for elem in row:
-#         print(elem, end=' ')
-#     print()
+a = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
+for row in a:
+    for elem in row:
+        print(elem, end=' ')
+    print()
 #
 # list1 = ["cory", "edgar", "gel", "kuhu", "tanner"]
 #
@@ -26,10 +26,5 @@
 #
 # print(sorted(list1))
 
-list1 = [1,2,3,4]
-tuple1 = (1,2,3,4,5)
-
-list1.extend(list(tuple1))
-
-print(tuple1)
-print(list1)
+# list1 = list('aeeiouu')
+# print(list1)
--- beersong.py
+++ beersong.py
@@ -9,7 +9,7 @@ for beer_num in range(99, 0, -1):
     if beer_num == 1:
         print('No more bottles of beer on the wall.')
     else:
-        new_num = beer_num -1
+        new_num = beer_num - 1
         if new_num == 1:
             word = 'bottle'
         print(new_num, word, 'of beer on the wall.')
