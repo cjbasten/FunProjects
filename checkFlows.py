5eeaf059fa31463736bacdcd9af2530421b4e666
corybasten <coryjbasten@gmail.com>
2019-01-06 17:10:36 -0800

    Commits

--- checkFlows.py
+++ checkFlows.py
@@ -1,7 +1,21 @@
-with open("madlib.txt") as hello:
-    newmadlib = hello.read()
+import time
 
-with open("newmadlib.txt", 'a') as new:
-        new.write(newmadlib + '\n\n')
 
+def check_flows(max_time: int, write_interval: int, original_file, new_file):
+    start_time = time.time()  # remember when we started
+    while (time.time() - start_time) < max_time:
+        with open(original_file) as hello:
+            new_mad_lib = hello.read()
+
+        with open(new_file, 'a') as new:
+            new.write('Time: ' + str(time.time()) + '\n' + new_mad_lib + '\n\n')
+        print(new_mad_lib)
+        time.sleep(write_interval)
+
+
+orig_file = input('Which file are you reading from?')
+dest_file = input('Which file are writing to?')
+timer = int(input('Enter the amount of seconds you want to run this: '))
+interval = int(input('Enter the seconds you would like between each write to the new file: '))
+check_flows(timer, interval, orig_file, dest_file,)
 
