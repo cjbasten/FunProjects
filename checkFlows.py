06f6c30dc7a35ec51578ac772c0945ee542d8340
corybasten <coryjbasten@gmail.com>
2019-01-06 21:20:21 -0800

    Commits

--- checkFlows.py
+++ checkFlows.py
@@ -1,7 +1,8 @@
 import time
+from threading import Thread
 
 
-def check_flows(max_time: int, write_interval: int, original_file, new_file):
+def read_file(max_time: int, write_interval: int, original_file, new_file):
     start_time = time.time()  # remember when we started
     while (time.time() - start_time) < max_time:
         with open(original_file) as hello:
@@ -13,9 +14,13 @@ def check_flows(max_time: int, write_interval: int, original_file, new_file):
         time.sleep(write_interval)
 
 
-orig_file = input('Which file are you reading from?')
-dest_file = input('Which file are writing to?')
-timer = int(input('Enter the amount of seconds you want to run this: '))
-interval = int(input('Enter the seconds you would like between each write to the new file: '))
-check_flows(timer, interval, orig_file, dest_file,)
+test_num = int(input('How many files would you like to check?'))
+for i in range(test_num):
+    thread_num = {}
+    orig_file = input('Which file are you reading from?')
+    dest_file = input('Which file are writing to?')
+    timer = int(input('Enter the amount of seconds you want to run this: '))
+    interval = int(input('Enter the seconds you would like between each write to the new file: '))
+    t = Thread(target=read_file, args=(timer, interval, orig_file, dest_file,))
+    t.start()
 
